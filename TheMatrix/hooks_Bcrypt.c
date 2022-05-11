3881a0d8f9d05544067bfee5944766a324cbe82c
Antonio 's4tan' Parata <aparata@gmail.com>
2022-05-11 19:59:00 +0200

    Removed functions for logging

--- TheMatrix/hooks_Bcrypt.c
+++ TheMatrix/hooks_Bcrypt.c
@@ -6,10 +6,7 @@ static hook_info* g_BCryptEncrypt_hook = 0;
 static hook_info* g_BCryptDecrypt_hook = 0;
 
 int __cdecl hook_BCryptDecrypt(BCRYPT_KEY_HANDLE hKey, PUCHAR pbInput, ULONG cbInput, VOID* pPaddingInfo, PUCHAR pbIV, ULONG cbIV, PUCHAR pbOutput, ULONG cbOutput, ULONG* pcbResult, ULONG dwFlags)
-{
-	// save clear text data
-	hook_log_data(g_BCryptEncrypt_hook, cbInput, pbInput);
-	
+{	
 	NTSTATUS ret = (NTSTATUS)hook_call_original(
 		g_BCryptDecrypt_hook,
 		hKey,
@@ -28,9 +25,6 @@ int __cdecl hook_BCryptDecrypt(BCRYPT_KEY_HANDLE hKey, PUCHAR pbInput, ULONG cbI
 
 int __cdecl hook_BCryptEncrypt(BCRYPT_KEY_HANDLE hKey, PUCHAR pbInput, ULONG cbInput, VOID* pPaddingInfo, PUCHAR pbIV, ULONG cbIV, PUCHAR pbOutput, ULONG cbOutput, ULONG* pcbResult, ULONG dwFlags)
 {
-	// save clear text data
-	hook_log_data(g_BCryptEncrypt_hook, cbInput, pbInput);
-
 	NTSTATUS ret = (NTSTATUS)hook_call_original(
 		g_BCryptEncrypt_hook,
 		hKey,
@@ -53,10 +47,7 @@ int hooks_bcrypt(void)
 	if (SHGetFolderPathA(HWND_DESKTOP, CSIDL_DESKTOP, NULL, SHGFP_TYPE_DEFAULT, log_directory) == S_OK) {
 		strcat_s(log_directory, sizeof log_directory, "\\");
 		g_BCryptEncrypt_hook = hook_add("Bcrypt.dll", "BCryptEncrypt", hook_BCryptEncrypt);
-		hook_set_log_folder(g_BCryptEncrypt_hook, log_directory);
-
 		g_BCryptDecrypt_hook = hook_add("Bcrypt.dll", "BCryptDecrypt", hook_BCryptDecrypt);
-		hook_set_log_folder(g_BCryptDecrypt_hook, log_directory);
 	}
 
 	return 0;
