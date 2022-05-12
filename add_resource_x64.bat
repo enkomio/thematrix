65b2eb0aa997e0556d5e91b6ce5d79ac6ff1e23f
Antonio 's4tan' Parata <aparata@gmail.com>
2022-05-13 00:11:22 +0200

    Created a custom test program

--- add_resource_x64.bat
+++ add_resource_x64.bat
@@ -1,3 +1,3 @@
-C:\Workspace\TheMatrix\Baseline\build\Debug\x64\TheMatrix.exe -add "C:\Workspace\TheMatrix\Misc\blobrunner_x64.exe"
+C:\Workspace\TheMatrix\Baseline\build\Debug\x64\TheMatrix.exe -add "C:\Workspace\TheMatrix\Misc\Bins\TestConsole_x64.exe"
 del C:\Workspace\TheMatrix\Baseline\build\Debug\x64\TheMatrix.exe
 ren C:\Workspace\TheMatrix\Baseline\build\Debug\x64\TheMatrix.build.exe TheMatrix.exe
--- add_resource_x86.bat
+++ add_resource_x86.bat
@@ -1,3 +1,3 @@
-C:\Workspace\TheMatrix\Baseline\build\Debug\x86\TheMatrix.exe -add "C:\Workspace\TheMatrix\Misc\blobrunner_x86.exe"
+C:\Workspace\TheMatrix\Baseline\build\Debug\x86\TheMatrix.exe -add "C:\Workspace\TheMatrix\Misc\Bins\TestConsole_x86.exe"
 del C:\Workspace\TheMatrix\Baseline\build\Debug\x86\TheMatrix.exe
 ren C:\Workspace\TheMatrix\Baseline\build\Debug\x86\TheMatrix.build.exe TheMatrix.exe
