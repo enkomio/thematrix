2fbad8bac39046777020eab00f135a3f07e373e1
Antonio 's4tan' Parata <aparata@gmail.com>
2022-05-12 23:42:15 +0200

    Added local test files

--- add_resource_x64.bat
+++ add_resource_x64.bat
@@ -1,3 +1,3 @@
- C:\Workspace\TheMatrix\Baseline\build\Debug\x64\TheMatrix.exe -add "C:\Workspace\Alan\Baseline\src\client\build\x64-Release\pe_packer_dll.dll"
+C:\Workspace\TheMatrix\Baseline\build\Debug\x64\TheMatrix.exe -add "C:\Workspace\TheMatrix\Misc\blobrunner_x64.exe"
 del C:\Workspace\TheMatrix\Baseline\build\Debug\x64\TheMatrix.exe
 ren C:\Workspace\TheMatrix\Baseline\build\Debug\x64\TheMatrix.build.exe TheMatrix.exe
--- add_resource_x86.bat
+++ add_resource_x86.bat
@@ -1,3 +1,3 @@
-C:\Workspace\TheMatrix\Baseline\build\Debug\x86\TheMatrix.exe -add "C:\Workspace\Alan\Baseline\src\client\build\x86-Release\pe_packer_dll.dll"
+C:\Workspace\TheMatrix\Baseline\build\Debug\x86\TheMatrix.exe -add "C:\Workspace\TheMatrix\Misc\blobrunner_x86.exe"
 del C:\Workspace\TheMatrix\Baseline\build\Debug\x86\TheMatrix.exe
 ren C:\Workspace\TheMatrix\Baseline\build\Debug\x86\TheMatrix.build.exe TheMatrix.exe
