4b3c5f59e31bfc0fd475197a613194a2c7b9a05e
Antonio 's4tan' Parata <aparata@gmail.com>
2022-05-14 00:01:35 +0200

    Completed hook engine x64

--- TheMatrix/hooks.h
+++ TheMatrix/hooks.h
@@ -9,12 +9,6 @@ typedef struct hook_info hook_info;
 extern hook_info* hook_add(char* dll_name, char* func_name, void (__cdecl *hook_func)());
 
 // call the original function
-extern  void* hook_call_original(hook_info* hook, ...);
-
-// log the specific data to an hardcoded directory
-extern void hook_log_data(hook_info* hook, size_t data_size, uint8_t* data);
-
-// set the output log folder to write logs
-extern void hook_set_log_folder(hook_info* hook, char* folder);
+extern void* hook_call_original(hook_info* hook, ...);
 
 #endif
--- TheMatrix/hooks_kernel32.c
+++ TheMatrix/hooks_kernel32.c
@@ -1,16 +1,66 @@
 #include <Windows.h>
 #include "hooks.h"
+#include "utility.h"
 
 static hook_info* g_VirtualFree_hook = 0;
+static hook_info* g_VirtualAlloc_hook = 0;
+
+typedef struct mem_allocated mem_allocated;
+struct mem_allocated {
+	LPVOID address;
+	DWORD size;
+	mem_allocated* next;
+};
+
+static mem_allocated* g_mem_root = 0;
+static mem_allocated* g_mem_tail = 0;
+
+LPVOID __cdecl VirtualAlloc_hook(LPVOID lpAddress, SIZE_T dwSize, DWORD flAllocationType, DWORD flProtect)
+{
+	LPVOID res = hook_call_original(g_VirtualAlloc_hook, lpAddress, dwSize, flAllocationType, flProtect);
+	if (res) {
+		// save details of the allocation size
+		if (!g_mem_root) {
+			g_mem_root = calloc(1, sizeof(mem_allocated));
+			if (g_mem_root) {
+				g_mem_root->address = res;
+				g_mem_root->size = dwSize;
+				g_mem_tail = g_mem_root;
+			}
+		}
+		else {
+			g_mem_tail->next = calloc(1, sizeof(mem_allocated));
+			if (g_mem_tail->next) {
+				g_mem_tail = g_mem_tail->next;
+				g_mem_tail->address = res;
+				g_mem_tail->size = dwSize;
+			}
+		}
+	}
+	return res;
+}
 
 LPVOID __cdecl VirtualFree_hook(LPVOID lpAddress, SIZE_T dwSize, DWORD dwFreeType)
 {
-	LPVOID allocated_buffer = hook_call_original(g_VirtualFree_hook, lpAddress, dwSize, dwFreeType);
-	return allocated_buffer;
+	// find the allocation info and save content to file
+	mem_allocated* m = g_mem_root;
+	while (m) {
+		if (m->address == lpAddress) {
+			char name[MAX_PATH] = { 0 };
+			snprintf(name, sizeof(name), "mem_%x_%d", lpAddress, m->size);
+			log_data(m->size, lpAddress, name);
+			break;
+		}
+		m = m->next;
+	}
+
+	LPVOID res = hook_call_original(g_VirtualFree_hook, lpAddress, dwSize, dwFreeType);
+	return res;
 }
 
 int hooks_kernel32(void)
 {
 	g_VirtualFree_hook = hook_add("Kernel32.dll", "VirtualFree", VirtualFree_hook);
+	g_VirtualAlloc_hook = hook_add("Kernel32.dll", "VirtualAlloc", VirtualAlloc_hook);
 	return 0;
 }
