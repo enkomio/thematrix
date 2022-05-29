c3780bac9d8adc12770fdbe6b23943a0c5e59f38
Antonio 's4tan' Parata <aparata@gmail.com>
2022-05-29 19:00:15 +0200

    Fixed error in string length computation

--- add_resource_x86.bat
+++ add_resource_x86.bat
@@ -1,4 +1,3 @@
-goto :eof
 C:\Workspace\TheMatrix\Baseline\build\TheMatrix\Debug\x86\TheMatrix.exe -add "C:\Workspace\TheMatrix\Baseline\build\ConsoleExe1_Test\Debug\x86\ConsoleExe1_Test.exe"
 del C:\Workspace\TheMatrix\Baseline\build\TheMatrix\Debug\x86\TheMatrix.exe
 ren C:\Workspace\TheMatrix\Baseline\build\TheMatrix\Debug\x86\TheMatrix.build.exe TheMatrix.exe
