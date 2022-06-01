02c35d3580a4e036e4d55e98909282d231e5ff39
Antonio 's4tan' Parata <aparata@gmail.com>
2022-06-02 00:40:41 +0200

    Fixed x64 hook engine and refactoring

--- TheMatrix/hooks.c
+++ TheMatrix/hooks.c
@@ -6,15 +6,18 @@
 extern void hooks_kernel32(void);
 extern void hooks_bcrypt(void);
 
+#ifndef _WIN64
 // pointer to function that calls the original function
 extern void* g_hook_call_original;
 
 __declspec(naked) LPVOID __stdcall call_original(void) {
 	((LPVOID(*)())g_hook_call_original)();
 }
+#endif
 
 bool hooks_init(uint8_t* hMod)
 {
+	// add function hooks here
 	hooks_kernel32();
 	hooks_bcrypt();
 	return true;
--- TheMatrix/hooks.h
+++ TheMatrix/hooks.h
@@ -8,7 +8,11 @@ typedef struct hook_info hook_info;
 // hook a specific Windows function
 extern hook_info* hook_add(char* dll_name, char* func_name, void* (__stdcall *hook_func)());
 
+#ifdef _WIN64
+extern LPVOID call_original(void);
+#else
 // call the original function
 LPVOID __stdcall call_original(void);
+#endif
 
 #endif
