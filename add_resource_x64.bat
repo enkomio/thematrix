cadc895998b028c121c4dc6405eb7acab12469d0
Antonio 's4tan' Parata <aparata@gmail.com>
2022-08-29 13:09:53 +0200

    Removed hard-coded pathes

--- add_resource_x64.bat
+++ add_resource_x64.bat
@@ -1,3 +1,3 @@
-C:\Workspace\TheMatrix\Baseline\build\TheMatrix\Debug\x64\TheMatrix.exe -add "C:\Workspace\TheMatrix\Baseline\build\ConsoleExe1_Test\Debug\x64\ConsoleExe1_Test.exe"
-del C:\Workspace\TheMatrix\Baseline\build\TheMatrix\Debug\x64\TheMatrix.exe
-ren C:\Workspace\TheMatrix\Baseline\build\TheMatrix\Debug\x64\TheMatrix.build.exe TheMatrix.exe
+..\build\TheMatrix\Debug\x64\TheMatrix.exe -add "..\build\ConsoleExe1_Test\Debug\x64\ConsoleExe1_Test.exe"
+del ..\build\TheMatrix\Debug\x64\TheMatrix.exe
+ren ..\build\TheMatrix\Debug\x64\TheMatrix.build.exe TheMatrix.exe
--- add_resource_x86.bat
+++ add_resource_x86.bat
@@ -1,3 +1,3 @@
-C:\Workspace\TheMatrix\Baseline\build\TheMatrix\Debug\x86\TheMatrix.exe -add "C:\Workspace\TheMatrix\Baseline\build\ConsoleExe1_Test\Debug\x86\ConsoleExe1_Test.exe"
-del C:\Workspace\TheMatrix\Baseline\build\TheMatrix\Debug\x86\TheMatrix.exe
-ren C:\Workspace\TheMatrix\Baseline\build\TheMatrix\Debug\x86\TheMatrix.build.exe TheMatrix.exe
+..\build\TheMatrix\Debug\x86\TheMatrix.exe -add "..\ConsoleExe1_Test\Debug\x86\ConsoleExe1_Test.exe"
+del ..\build\TheMatrix\Debug\x86\TheMatrix.exe
+ren ..\build\TheMatrix\Debug\x86\TheMatrix.build.exe TheMatrix.exe
