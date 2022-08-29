99eba9b77819f5290209f0523d2b297807031da7
Antonio 's4tan' Parata <aparata@gmail.com>
2022-08-29 23:48:06 +0200

    Finalized x86 PE loader

--- add_resource_x64.bat
+++ add_resource_x64.bat
@@ -1,3 +1,10 @@
+del ..\build\TheMatrix\Debug\x64\TheMatrix.dll
+
+rem Create a binary to test DLL
+..\build\TheMatrix\Debug\x64\TheMatrix.exe -add "..\ConsoleExe1_Test\bins\adprovider.dll"
+ren ..\build\TheMatrix\Debug\x64\TheMatrix.build.dll TheMatrix.dll
+
+rem Create a binary to test EXE
 ..\build\TheMatrix\Debug\x64\TheMatrix.exe -add "..\ConsoleExe1_Test\Debug\x64\ConsoleExe1_Test.exe"
 del ..\build\TheMatrix\Debug\x64\TheMatrix.exe
 ren ..\build\TheMatrix\Debug\x64\TheMatrix.build.exe TheMatrix.exe
--- add_resource_x86.bat
+++ add_resource_x86.bat
@@ -1,3 +1,10 @@
+del ..\build\TheMatrix\Debug\x86\TheMatrix.dll
+
+rem Create a binary to test DLL
+..\build\TheMatrix\Debug\x86\TheMatrix.exe -add "..\ConsoleExe1_Test\bins\xapauthenticodesip.dll"
+ren ..\build\TheMatrix\Debug\x86\TheMatrix.build.dll TheMatrix.dll
+
+rem Create a binary to test EXE
 ..\build\TheMatrix\Debug\x86\TheMatrix.exe -add "..\ConsoleExe1_Test\Debug\x86\ConsoleExe1_Test.exe"
 del ..\build\TheMatrix\Debug\x86\TheMatrix.exe
 ren ..\build\TheMatrix\Debug\x86\TheMatrix.build.exe TheMatrix.exe
