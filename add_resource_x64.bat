2c9a23874cd87bc6e4c8d9b784dc338ba24ae43a
Antonio 's4tan' Parata <aparata@gmail.com>
2022-08-29 13:11:46 +0200

    Fixed path

--- add_resource_x64.bat
+++ add_resource_x64.bat
@@ -1,3 +1,3 @@
-..\build\TheMatrix\Debug\x64\TheMatrix.exe -add "..\build\ConsoleExe1_Test\Debug\x64\ConsoleExe1_Test.exe"
+..\build\TheMatrix\Debug\x64\TheMatrix.exe -add "..\ConsoleExe1_Test\Debug\x64\ConsoleExe1_Test.exe"
 del ..\build\TheMatrix\Debug\x64\TheMatrix.exe
 ren ..\build\TheMatrix\Debug\x64\TheMatrix.build.exe TheMatrix.exe
